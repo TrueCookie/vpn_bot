c53ddbf9e9bbc2d100d2959c3af25a7a0f3428fb
artemiy.ogloblin <artemiy.ogloblin@CPR-PF2XNN38>
2023-10-25 01:39:39 +0300

    РЕФАКТОР: Разделены методы: 1. Вызова скриптов bash; 2. Внутренние методы бота; 3.

--- _true_bot.py
+++ _true_bot.py
@@ -10,32 +10,76 @@ bot = telebot.TeleBot('6180127098:AAHn1CEOf4LKlvx3K36lyLJT2vfD2iGAFN0')
 # приветственный текст
 start_txt = 'Привет! Это журнал «Код». \n\nТеперь у бота появился бэкенд.'
 
+# ВЫЗОВ СКРИПТОВ BASH:
+def reg_client(client_id):
+    # 1.регистрируем клиента
+    try:
+        subprocess.run(["sh", "/root/dev/ovpn_serv/reg_client.sh", client_id])
+    except Exception as e:
+        print('Не удалось выполнить bash-скрипт:', e)
+        raise
 
+def new_client(client_id):
+    # 2.создаём файл конфигурации клиента
+    try:
+        subprocess.run(["sh", "/root/dev/ovpn_serv/new_client.sh", client_id])
+    except Exception as e:
+        print('Не удалось выполнить bash-скрипт:', e)
+        raise
+
+def revoke_client(client_id):
+    try:
+        subprocess.run(["sh", "/root/dev/ovpn_serv/revoke.sh", client_id])
+    except Exception as e:
+        print('Не удалось выполнить bash-скрипт:', e)
+        raise
+
+def renew_client(client_id):
+    try:
+        subprocess.run(["sh", "/root/dev/ovpn_serv/renew.sh", client_id])
+    except Exception as e:
+        print('Не удалось выполнить bash-скрипт:', e)
+        raise
+
+# ВНУТРЕННИЕ МЕТОДЫ БОТА:
+# Отправить сертификат клиента
+@bot.message_handler(commands=['connect'], content_types=['text'])
+def send_cert(message, client_id):
+    try:
+        # 3.высылаем файл
+        bot.send_message(chat_id=message.from_user.id, 
+                        text="""Новый сертификат создан!
+                                Осталось только добавить его в приложение""")
+        bot.send_document(message.chat.id, open(rf"/root/{client_id}.ovpn", 'rb'))
+    except Exception as e:
+        bot.send_message(chat_id=message.from_user.id, text=f"Нового клиента создать не удалось :(.\n{e}")
+
+
+# ОБРАБОТКА КОМАНД:
 # КОМАНДА: /start
 @bot.message_handler(commands=['start'])
 def start(message):
     # выводим приветственное сообщение
     bot.send_message(message.from_user.id, start_txt, parse_mode='Markdown')
 
-# КОМАНДА: /connect
-@bot.message_handler(commands=['connect'], content_types=['text'])
-def send_file(message):
+# ДЕЙСТВИЯ БОТА:
+# КОМАНДА: /register
+@bot.message_handler(commands=['register'])
+def new_user(message):
     try:
+        # TODO: Заменить на читаемое имя (client_N)
         # 0.генерируем id клиента
         client_id = str(uuid.uuid4())
 
-        # 1.регистрируем клиента
-        subprocess.run(["sh", "/root/dev/ovpn_serv/reg_client.sh", client_id])
-        
-        # 2.создаём файл конфигурации клиента
-        subprocess.run(["sh", "/root/dev/ovpn_serv/new_client.sh", client_id])
-        bot.send_message(chat_id=message.from_user.id, text="Новый клиент создан!")
-    
-        # 3.высылаем файл
-        bot.send_document(message.chat.id, open(rf"/root/{client_id}.ovpn", 'rb'))
+        reg_client(client_id)
+        new_client(client_id)
+        send_cert(message, client_id)
     except Exception as e:
         bot.send_message(chat_id=message.from_user.id, text=f"Нового клиента создать не удалось :(.\n{e}")
 
+# БЛОКИРОВКА ПОЛЬЗОВАТЕЛЯ:
+
+# ВОЗОБНОВЛЕНИЕ ПОЛЬЗОВАТЕЛЯ:
 
 # запускаем бота
 if __name__ == '__main__':
